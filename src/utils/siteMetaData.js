5a8390fe1a24372367aa4e1564a861b30f811f93
rizwan ishaq <rizwanishaq@gmail.com>
2023-09-21 16:06:06 +0200

    adding

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,12 +1,12 @@
 // please update required information
 const siteMetadata = {
-  title: "Next.js Blog With Tailwind CSS and Contentlayer",
+  title: "RiIsBiTech Blogs",
   author: "Rizwan Ishaq",
   headerTitle: "RiIsBiTech Blogs",
   description: "Blogs to learn and share knowledge with others.",
   language: "en-us",
   theme: "system", // system, dark or light
-  siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+  siteUrl: "https://vercel.com/rizwanishaq/blogs-personal", // your website URL
   siteLogo: "/logo.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   email: "rizwanishaq@gmail.com",
